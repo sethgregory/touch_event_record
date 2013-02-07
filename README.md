6363c77650ab4ec0c929d3770e48d9e918f625cf
Seth Gregory <sethgregory@gmail.com>
2013-02-07 11:18:56 -0500

    added ability to write to output file

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ and does the appropriate conversions, printing out the corresponding
 adb shell sendevent commands to replay your touch actions.
 
 After completing your input, you can copy the output into a shell script
+(or, optionally, use the commandline argument to specify an output file)
 and add sleep statements as needed to introduce delays between touch
 events in playback.  (A later version may automatically create these, but
 for now you're on your own ;))
@@ -14,4 +15,13 @@ for now you're on your own ;))
 
 Usage: adb shell getevent | touch_event_record.py
 
+    The following options are available:
+
+    -h    Print usage summary
+
+    -o    Specify an output file (e.g., -o outfile.txt)
+
+    -e    Specify the appropriate event name for your device's touchscreen,
+          (e.g., -e event1) - you can find this by running adb shell getevent
+          and reading the top of the output.
 
--- touch_event_record.py
+++ touch_event_record.py
@@ -13,36 +13,69 @@ adb shell sendevent commands to replay your touch actions.
 
 import sys
 import string
+import getopt
 
-# This value may vary between devices.  Run an adb shell getevent and look at
-# the list of event types.  Set this variable to the appropriate one (e.g.,
-# mine looks like this:
-#         add device 6: /dev/input/event1
-#           name:     "Melfas MMSxxx Touchscreen"
-event = 'event1'
 
-# Keep a count of how many captured events we collect, just for fun.
-count = 0
+def main(argv):
 
-# Watch the buffer and convert any matching items
-try:
-  buff = ''
-  while True:
-    buff += sys.stdin.read(1)
-    if buff.endswith('\n'):
-      if(buff.startswith('/dev/input/' + event)):
-        linelist = string.split(buff[:-1])
-        linelist[0] = 'adb shell sendevent /dev/input/' + event
-        linelist[1] = str(int(linelist[1], 16))
-        linelist[2] = str(int(linelist[2], 16))
-        linelist[3] = str(int(linelist[3], 16))
-        output = ' '.join(linelist)
-        print output
-        count = count + 1
-      buff = ''
-      
-# Catch KeyboardInterrupt exception on CTRL+C
-except KeyboardInterrupt:
-  sys.stdout.flush()
-  pass
-  print '\nExiting.  ' + str(count) + ' commands captured.'
+  # This value may vary between devices.  Run an adb shell getevent and look at
+  # the list of event types.  Set this variable to the appropriate one (e.g.,
+  # mine looks like this:
+  #         add device 6: /dev/input/event1
+  #           name:     "Melfas MMSxxx Touchscreen"
+  event = 'event1'
+  
+  # Keep a count of how many captured events we collect, just for fun.
+  count = 0
+
+  # No file output by default
+  outputfile = ''
+
+
+  # Get and parse commandline args/options  
+  try:
+    opts, args = getopt.getopt(argv,"he:o:",["event=", "output="])
+  except getopt.GetoptError:
+    print 'touch_event_record.py [-d <eventnum>] [-o <outputfile>]'
+    sys.exit(2)
+  for opt, arg in opts:
+    if opt == '-h':
+      print 'touch_event_record.py [-d <eventnum>] [-o <outputfile>]'
+      sys.exit()
+    elif opt in ("-e", "--event"):
+      event = arg
+    elif opt in ("-o", "--output"):
+      outputfile = arg
+      file = open(outputfile, 'w')
+
+  # Watch the buffer and convert any matching items
+  try:
+    buff = ''
+    while True:
+      buff += sys.stdin.read(1)
+      if buff.endswith('\n'):
+        if(buff.startswith('/dev/input/' + event)):
+          linelist = string.split(buff[:-1])
+          linelist[0] = 'adb shell sendevent /dev/input/' + event
+          linelist[1] = str(int(linelist[1], 16))
+          linelist[2] = str(int(linelist[2], 16))
+          linelist[3] = str(int(linelist[3], 16))
+          output = ' '.join(linelist)
+          print output
+          if outputfile != '':
+            file.write(output + '\n')
+          count = count + 1
+        buff = ''
+        
+  # Catch KeyboardInterrupt exception on CTRL+C
+  except KeyboardInterrupt:
+    sys.stdout.flush()
+
+    if outputfile != '':
+      file.close()
+
+    pass
+    print '\nExiting.  ' + str(count) + ' commands captured.'
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
